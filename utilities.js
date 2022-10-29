986e9bf4bfb0ebd482796854983c11afd9f47bf0
Mac <90430359+mackenzietan@users.noreply.github.com>
2022-10-28 21:27:07 -0400

    Delete utilities.js
    removed from main project folder, moved to modules folder

--- utilities.js
+++
@@ -1,7 +0,0 @@
-export const formatNumber = number => {
-    let numString = String(Math.floor(number)); //convert number to string, round to nearest whole dollar
-    for (let i = numString.length - 3; i > 0; i -= 3) { //for every interval of three from the end, so long as the interval is greater than 0
-        numString = numString.slice(0,i) + ',' + numString.slice(i); //slice at specified interval and insert ','
-    return numString;
-    }
-};
